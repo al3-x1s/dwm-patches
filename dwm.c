6d67dc1396acff8b1f0d9a1850b1f51f73ebd994
al3-x1s <al3x1s.404@gmail.com>
2025-02-04 09:49:30 -0300

    patch-statusallmons

--- dwm.c
+++ dwm.c
@@ -731,7 +731,7 @@ drawbar(Monitor *m)
 		return;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == selmon || 1) { /* status is only drawn on selected monitor */
 		char *text, *s, ch;
 		drw_setscheme(drw, scheme[SchemeNorm]);
 
@@ -2097,6 +2097,7 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
+	Monitor* m;
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext))) {
 		strcpy(stext, "dwm-"VERSION);
 		statusw = TEXTW(stext) - lrpad + 2;
@@ -2116,7 +2117,10 @@ updatestatus(void)
 		statusw += TEXTW(text) - lrpad + 2;
 
 	}
-	drawbar(selmon);
+	
+	for(m = mons; m; m = m->next)
+		drawbar(m);
+
 }
 
 void
